73989daee6eed7a3aeb2d3060884428d42d19fee
Santigallego10 <90339673+Santigallego10@users.noreply.github.com>
2022-06-10 12:04:46 -0500

    Lista de usuarios cargada en  front
    Pendiente de agregar la barra de busqueda de usuarios

--- subastasYaFront/src/app/users/user-list.component.ts
+++ subastasYaFront/src/app/users/user-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../models/usuario';
+import { UsuarioService } from '../service/usuario.service';
+import { ToastrService } from 'ngx-toastr';
+import { TokenService } from '../service/token.service';
+
 
 @Component({
   selector: 'app-user-list',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  usuarios: Usuario[] = [];
+  roles: string[] = [];
+  isAdmin = false;
+
+  constructor(
+    private usuarioService: UsuarioService,
+    private toastr: ToastrService,
+    private tokenService: TokenService
+  ) { }
 
   ngOnInit() {
+    this.usuarioService.lista().subscribe(
+      data => this.usuarios = data
+    )
+    this.roles = this.tokenService.getAuthorities();
+    this.roles.forEach((rol) => {
+      if (rol === 'ROLE_ADMIN') {
+        this.isAdmin = true;
+      }
+    });
   }
 
 }
