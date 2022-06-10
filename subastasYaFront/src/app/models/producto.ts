391ffcd9ab0aa5d02d5f29e1c50c2e84601cbfb8
Santigallego10 <90339673+Santigallego10@users.noreply.github.com>
2022-06-10 09:50:17 -0500

    LISTA DE ANUNCIOS ADMIN

--- subastasYaFront/src/app/models/producto.ts
+++ subastasYaFront/src/app/models/producto.ts
@@ -1,9 +1,13 @@
+import { TipoProducto } from "./tipoProducto";
+
 export class Producto {
     id?: number = 0;
     nombre: string = '';
+    tipoProdcucto: TipoProducto | undefined;
 
-    constructor(nombre: string) {
+    constructor(nombre: string, tipoProducto: TipoProducto) {
         this.nombre = nombre;
+        this.tipoProdcucto = tipoProducto;
     }
 }
 
