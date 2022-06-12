e42036e691634e188f57e1e77f5e914345047182
Santiago Jaramillo López <santiagojaralopez@gmail.com>
2022-06-12 00:45:09 -0500

    Changed enum values to String

--- co.edu.cue.subastasYa/src/main/java/co/edu/cue/subastasYa/security/enums/TipoDocumento.java
+++ co.edu.cue.subastasYa/src/main/java/co/edu/cue/subastasYa/security/enums/TipoDocumento.java
@@ -2,5 +2,18 @@ package co.edu.cue.subastasYa.security.enums;
 
 
 public enum TipoDocumento {
-    PASAPORTE, CEDULA, CEDULAEXTRANJERIA
+    PASAPORTE("Pasaporte"),
+    CEDULA_DE_CIUDADANIA("Cedula de Ciudadania"),
+    CEDULA_DE_EXTRANJERIA("Cedula de Extranjeria");
+
+    private final String text;
+
+    TipoDocumento(final String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
 }
