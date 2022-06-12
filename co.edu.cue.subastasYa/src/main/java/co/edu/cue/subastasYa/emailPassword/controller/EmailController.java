41a46c89396b932a8dcb2be9e606b7fe0aadd457
Santiago Jaramillo López <santiagojaralopez@gmail.com>
2022-06-12 01:26:14 -0500

    Chenged response messages

--- co.edu.cue.subastasYa/src/main/java/co/edu/cue/subastasYa/emailPassword/controller/EmailController.java
+++ co.edu.cue.subastasYa/src/main/java/co/edu/cue/subastasYa/emailPassword/controller/EmailController.java
@@ -66,14 +66,14 @@ public class EmailController {
     @PostMapping("/change-password")
     public ResponseEntity<?> changePassword(@Valid @RequestBody ChangePasswordDTO dto, BindingResult bindingResult) {
         if (bindingResult.hasErrors())
-            return new ResponseEntity(new Mensaje("Campos mal puestos"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity(new Mensaje("Campos mal diligenciados"), HttpStatus.BAD_REQUEST);
         if (!dto.getPassword().equals(dto.getConfirmPassword()))
             return new ResponseEntity(new Mensaje("Las contraseñas no coinciden"), HttpStatus.BAD_REQUEST);
 
         Optional<Usuario> usuarioOpt = usuarioService.getByTokenPassword(dto.getTokenPassword());
 
         if (!usuarioOpt.isPresent())
-            return new ResponseEntity(new Mensaje("No existe ningún usuario con esas credenciales"), HttpStatus.NOT_FOUND);
+            return new ResponseEntity(new Mensaje("Parece que esta sesión ha expirado, intenta de nuevo"), HttpStatus.NOT_FOUND);
 
         Usuario usuario = usuarioOpt.get();
         String newPassword = passwordEncoder.encode(dto.getPassword());
