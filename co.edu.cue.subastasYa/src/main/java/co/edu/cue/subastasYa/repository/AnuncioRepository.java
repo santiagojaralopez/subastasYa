646b15fbd2d85d1a1e9d935995ecf472a6d4b149
Santiago Jaramillo López <santiagojaralopez@gmail.com>
2022-06-13 10:47:30 -0500

    Added query method findAnuncioByUsuario

--- co.edu.cue.subastasYa/src/main/java/co/edu/cue/subastasYa/repository/AnuncioRepository.java
+++ co.edu.cue.subastasYa/src/main/java/co/edu/cue/subastasYa/repository/AnuncioRepository.java
@@ -1,12 +1,14 @@
 package co.edu.cue.subastasYa.repository;
 
 import co.edu.cue.subastasYa.entity.Anuncio;
+import co.edu.cue.subastasYa.security.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
@@ -14,8 +16,12 @@ public interface AnuncioRepository extends JpaRepository<Anuncio, Integer>{
 
     @Query("SELECT a FROM Anuncio a WHERE a.estado= :estado")
     List<Anuncio> findAnuncioByEstado(
-            @Param("estado") int estado);
+            @Param("estado") int estado
+    );
 
-
+    @Query("SELECT a FROM Anuncio a WHERE a.usuario= :usuario")
+    Optional<Anuncio> findAnuncioByUsuario(
+            @Param("usuario") Usuario usuario
+    );
 
 }
