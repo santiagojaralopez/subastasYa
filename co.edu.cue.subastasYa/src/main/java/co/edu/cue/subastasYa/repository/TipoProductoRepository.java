ac8e18c57f2055037363200ded0117aeaeb1e0ac
Santigallego10 <90339673+Santigallego10@users.noreply.github.com>
2022-06-15 17:30:40 -0500

    Delete controller duplicado

--- co.edu.cue.subastasYa/src/main/java/co/edu/cue/subastasYa/repository/TipoProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package co.edu.cue.subastasYa.repository;
-
-import co.edu.cue.subastasYa.entity.TipoProducto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TipoProductoRepository extends JpaRepository<TipoProducto, Integer> {
-}
