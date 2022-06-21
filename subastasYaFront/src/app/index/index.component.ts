8fb11864eec1abb698a602cf8e64e117876374b5
Santiago Jaramillo López <santiagojaralopez@gmail.com>
2022-06-21 11:24:55 -0500

    Added offer pop-window and logic related to it

--- subastasYaFront/src/app/index/index.component.ts
+++ subastasYaFront/src/app/index/index.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { TokenService } from '../service/token.service';
 import { AnuncioService } from '../service/anuncio.service';
 import { Anuncio } from '../models/anuncio';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { OfferDTO } from '../models/offer-dto';
 
 @Component({
   selector: 'app-index',
@@ -17,16 +20,70 @@ export class IndexComponent implements OnInit {
   isLogged = false;
   nombreUsuario = '';
 
+  offerDto: OfferDTO = new OfferDTO();
+
   constructor(
     private anuncioService: AnuncioService,
+    private tokenService: TokenService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.anuncioService.listaActivos().subscribe(
       data => this.anuncios = data
-    )
-    console.log(this.anuncios)
-    
+    );
+
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    }
   }
 
+  onOffer() {
+    if (!this.isLogged) {
+      this.router.navigate(['/login']);
+    } else {
+      Swal.fire({
+        title: '<strong>Pujar por este producto</strong>',
+        input: 'number',
+        inputLabel: '¿Cuánto quieres pujar por este anuncio?',
+        inputValue: this.offerDto.offerValue,
+        showCloseButton: true,
+        showDenyButton: true,
+        focusConfirm: false,
+        confirmButtonText: 'Pujar',
+        denyButtonText: 'Cancelar',
+        inputValidator: (value) => {
+          if (!value) {
+            return 'Debes asignar un valor a tu puja!';
+          } else {
+            this.offerDto.offerValue = parseFloat(value);
+          }
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.sureAboutOffering();
+        } else if (result.isDenied) {
+          Swal.fire('Se ha cancelado tu proceso de puja', '', 'info');
+          this.offerDto.offerValue = undefined;
+        }
+      });
+    }
+  }
+
+  sureAboutOffering() {
+    Swal.fire({
+      title: 'Confirmar puja',
+      showDenyButton: true,
+      confirmButtonText: 'Confirmar',
+      denyButtonText: `Cancelar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire('Puja realizada!', 'Has pujado por un valor de ' + this.offerDto.offerValue, 'success');
+        console.log(this.offerDto.offerValue);
+      } else if (result.isDenied) {
+        Swal.fire('Se ha cancelado tu proceso de puja', '', 'info');
+        this.offerDto.offerValue = undefined;
+      }
+    });
+  }
 }
